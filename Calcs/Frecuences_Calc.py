9ec763fe1a8aeab0eea6f1606089f73b2f0711f3
JunixOs <yonelsilva75@gmail.com>
2025-02-02 20:15:31 -0500

    Añade los archivos principales del proyecto

---
+++ Calcs/Frecuences_Calc.py
@@ -0,0 +1,293 @@
+from math import *
+def Separate_Data(a):
+    number = ""
+    Data = []
+    for n in range(0,len(a)):
+        char = a[n]
+        if(char == " " or n==len(a)-1 or char=="\n"):
+            if(n==len(a)-1 and char!=" "):
+                number+=char
+
+            if(number==""):
+                continue
+
+            else:
+                Data.append(number)
+                number = ""
+
+        else:
+            number += char
+
+    return Data
+
+def Conv_Data(Data):
+    Data_Converted = Data
+    for n in range(0,len(Data)):
+        Bool = "." in Data_Converted[n]
+        match Bool:
+            case True:
+                Data_Converted[n] = float(Data_Converted[n])
+            case False:
+                Data_Converted[n] = int(Data_Converted[n])
+        if (Data_Converted[n]<0):
+            raise ValueError("Datos no procesados correctamente")
+    return Data_Converted
+
+def Calc_Decimals_Number(Number):
+    Temp = str(Number)
+    Count = 0
+    for a in range(len(Temp)-1,0,-1):
+        if(Temp[a] == "."):
+            return Count
+        else:
+            Count = Count + 1
+
+def Calc_Max(Data):
+    return max(Data)
+
+def Calc_Min(Data):
+    return min(Data)
+
+def Calc_Range(Min , Max):
+    return Max-Min
+
+def Calc_Intervals(Min , C , Max , m , Precision):
+    Arr_Intervals = [[0 for _ in range(2)] for _ in range(m)]
+    
+    Acumulate = Min
+    for a in range(0 , m):
+        for b in range(0 , 2):
+            if(b==0):
+                Arr_Intervals[a][b] = Acumulate
+            else:
+                Acumulate = Acumulate + C
+            
+            if(Acumulate>Max):
+                Acumulate=Max
+            if(Acumulate<Max and a==m-1 and b==1):
+                Acumulate=Max
+            
+            if(Precision!=0):
+                Arr_Intervals[a][b] = round(Acumulate , Precision)
+            else:
+                Arr_Intervals[a][b] = round(Acumulate)
+
+    return Arr_Intervals
+
+def Calc_Groups(Intervals , m , Precision):
+    Arr_Groups = []
+    for a in range (0 , m):
+        if(a==m-1):
+            Arr_Groups.append(Intervals[a][1])
+        else:
+            if(Precision==0):
+                Arr_Groups.append(Intervals[a][1] - 1)
+            else:
+                Arr_Groups.append(round(Intervals[a][1] - 1*pow(10,-1*Precision) , Precision))
+
+    return Arr_Groups
+
+def Calc_xi(Intervals , m , Precision):
+    Arr_xi = []
+    for a in range (0 , m):
+        if(Precision == 0):
+            Arr_xi.append((Intervals[a][1]-Intervals[a][0])/2)
+        else:
+            Arr_xi.append(round((Intervals[a][1]-Intervals[a][0])/2 , Precision))
+
+    return Arr_xi
+
+def Calc_fi(Data , Intervals , m):
+    Arr_fi = []
+    Count = 0
+    for n in range(0 , m):
+        for a in range(0 , len(Data)):
+            if(n==m-1):
+                if(Intervals[n][0] <= Data[a] <= Intervals[n][1]):
+                    Count += 1
+            else:
+                if(Intervals[n][0] <= Data[a] < Intervals[n][1]):
+                        Count += 1
+        Arr_fi.append(Count)
+        Count = 0
+    return Arr_fi
+
+def Calc_Fi(fi):
+    Arr_Fi = []
+    Acumulate = 0
+    for a in fi:
+        Acumulate += a
+        Arr_Fi.append(Acumulate)
+    return Arr_Fi
+
+def Calc_hi(Data , fi , Precision):
+    Arr_hi = []
+    N = len(Data)
+    for a in fi:
+        if(Precision != 0):
+            Arr_hi.append(round(a/N , Precision))
+        else:
+            Arr_hi.append(round(a/N))
+    return Arr_hi
+
+def Calc_Hi(hi , Precision):
+    Arr_Hi = []
+    Acumulate = 0
+    for a in hi:
+        Acumulate += a
+        if(Precision != 0):
+            Arr_Hi.append(round(Acumulate , Precision))
+        else:
+            Arr_Hi.append(round(Acumulate))
+    return Arr_Hi
+
+def Calc_hi_percent(hi , Precision):
+    Arr_hi_percent = []
+    for a in hi:
+        if(Precision !=0):
+            Arr_hi_percent.append(round(a*100 , Precision))
+        else:
+            Arr_hi_percent.append(round(a*100))
+    return Arr_hi_percent
+
+def Calc_Hi_percent(Hi , Precision):
+    Arr_Hi_percent = []
+    for a in Hi:
+        if(Precision != 0 ):
+            Arr_Hi_percent.append(round(a*100 , Precision))
+        else:
+            Arr_Hi_percent.append(round(a*100))
+    return Arr_Hi_percent
+
+def Print_Table_On_Window(m , Intervals , Arr_xi , Arr_fi , Arr_Fi , Arr_hi , Arr_Hi , Arr_hi_precent , Arr_Hi_percent):
+    print("   m    |     Li     |     Ls     |     xi    |     fi     |     Fi     |     hi     |     Hi     |     hi%     |     Hi%     ")
+    for a in range(m):
+        print("------------------------------------------------------------------------------------------------------------------------------")
+        print(f"  {a}    ")
+        print("------------------------------------------------------------------------------------------------------------------------------")
+    
+def Main_Function(N_Decimals_Precision , In):
+    if(__name__ == "__main__"):
+        """ In = input("Ingrese los valores separados por espacios, si son decimales evitar usar comas \",\", usar punto \".\": \n") """
+        In = """118
+                484
+                664
+                1004
+                1231
+                1372
+                1582
+                118
+                484
+                664
+                1004
+                1231
+                1372
+                1582
+                118
+                484
+                664
+                1004
+                1231
+                1372
+                1582
+                118
+                484
+                664
+                1004
+                1231
+                1372
+                1582
+                118
+                484
+                664
+                1004
+                1231
+                1372
+                1582"""
+
+    Data = Separate_Data(In)
+    Data = Conv_Data(Data)
+
+    if(not Data):
+        raise ValueError("Datos no procesados correctamente")
+    else:
+
+        V_Min = Calc_Min(Data)
+        V_Max = Calc_Max(Data)
+
+        n = len(Data)
+        R = Calc_Range(V_Min,V_Max)
+        m = round(1+(3.3*log10(n)))
+        C = R/m
+
+        C_N_Decimals = Calc_Decimals_Number(C) # N de decimales de la amplitud (C)
+        if(C - round(C)==0):
+            C = round(C)
+            C_N_Decimals = 0
+        elif(C_N_Decimals>3):
+            C = round(C , 3)
+        elif(C_N_Decimals>=1 and C_N_Decimals<=3):
+            C = round(C , C_N_Decimals)
+
+        
+        Arr_Intervals = Calc_Intervals(V_Min , C , V_Max , m , C_N_Decimals)
+        Arr_Groups = Calc_Groups(Arr_Intervals , m , C_N_Decimals)
+
+        Arr_xi = Calc_xi(Arr_Intervals , m , N_Decimals_Precision)
+
+        Arr_fi = Calc_fi(Data , Arr_Intervals , m)
+        Arr_Fi = Calc_Fi(Arr_fi)
+
+        Arr_hi = Calc_hi(Data , Arr_fi , N_Decimals_Precision)
+        Arr_Hi = Calc_Hi(Arr_hi , N_Decimals_Precision)
+
+        Arr_hi_percent = Calc_hi_percent(Arr_hi , N_Decimals_Precision)
+        Arr_Hi_percent = Calc_Hi_percent(Arr_Hi , N_Decimals_Precision)
+
+        if(__name__ == "__main__"):
+            print(f"Datos Ingresados: \n")
+            for a in Data:
+                print(f"{a} ")
+            print(f"Intervalos: {Arr_Intervals}")
+            print(f"Grupos: {Arr_Groups}")
+            print(f"xi: {Arr_xi}")
+            print(f"fi: {Arr_fi}")
+            print(f"Fi: {Arr_Fi}")
+            print(f"hi: {Arr_hi}")
+            print(f"Hi: {Arr_Hi}")
+
+            print(f"hi%: {Arr_hi_percent}")
+            print(f"Hi%: {Arr_Hi_percent}")
+
+            """ d3 = dict(Nombre='Sara',
+                Edad=27,
+                Documento=1003882) """
+        else:
+            Variables_Value = dict([
+                ("V_Max" , V_Max),
+                ("V_Min" , V_Min),
+                ("n" , n),
+                ("R" , R),
+                ("m" , m),
+                ("C" , C),
+            ])
+
+            Frecuences = dict(
+                Data_Values = Data,
+                Intervals = Arr_Intervals,
+                Groups = Arr_Groups,
+                xi = Arr_xi,
+                fi = Arr_fi,
+                Fi = Arr_Fi,
+                hi = Arr_hi,
+                Hi = Arr_Hi,
+                hi_percent = Arr_hi_percent,
+                Hi_percent = Arr_Hi_percent,
+            )
+
+            # print(Frecuences["Intervals"][0][0])    <--- Ingreso a Intervalos y busco el primer valor del primer intervalo
+            return Variables_Value , Frecuences
+
+if (__name__ == "__main__"):
+    N_Decimals = int(input("Ingrese la cantidad de decimales (precision): "))
+    Main_Function(N_Decimals , 0)
---
+++ Windows/Window.py
@@ -0,0 +1,184 @@
+import sys
+import os
+# Esto añade la carpeta raiz
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Calcs.Frecuences_Calc import *
+from tkinter import *
+from tkinter import ttk
+
+class TreeviewFrame(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hscrollbar = ttk.Scrollbar(self, orient=HORIZONTAL)
+        self.vscrollbar = ttk.Scrollbar(self, orient=VERTICAL)
+        self.treeview = ttk.Treeview(
+            self,
+            xscrollcommand=self.hscrollbar.set,
+            yscrollcommand=self.vscrollbar.set
+        )
+        self.hscrollbar.config(command=self.treeview.xview)
+        self.hscrollbar.pack(side=BOTTOM, fill=X)
+        self.vscrollbar.config(command=self.treeview.yview)
+        self.vscrollbar.pack(side=RIGHT, fill=Y)
+        self.treeview.pack(fill="both" , expand=True)
+
+    def has_rows(self):
+        return len(self.treeview.get_children()) > 0
+    
+    def clear_table(self):
+        if(self.has_rows()):
+            for item in self.treeview.get_children():
+                self.treeview.delete(item)
+
+def Create_Window_Frecuences_Table():
+    Main_Window.state(newstate="withdraw")
+
+    def Back_to_main_window():
+        Main_Window.state(newstate="normal")
+        Main_Window.geometry("1240x700+135+100")
+        Main_Window.title("Seleccion")
+        Window_Frecuences_Table.destroy()
+    def Create_Table(Precision , Input , Table_Frecuences):
+        try:
+            Table_Frecuences.clear_table()
+            
+            Input = str(Input.get())
+            Precision = int(Precision.get())
+            Variables , Frecuences = Main_Function(Precision, Input)
+        
+            for a in range(0 , Variables["m"]):
+                Table_Frecuences.treeview.insert(
+                    "", END , values=(
+                        a+1, 
+                        Frecuences["Intervals"][a][0], 
+                        Frecuences["Intervals"][a][1], 
+                        Frecuences["Groups"][a], 
+                        Frecuences["xi"][a],
+                        Frecuences["fi"][a],
+                        Frecuences["Fi"][a],
+                        Frecuences["hi"][a],
+                        Frecuences["Hi"][a],
+                        f"{Frecuences["hi_percent"][a]}%",
+                        f"{Frecuences["Hi_percent"][a]}%",)
+                )
+            for col in Table_Frecuences.treeview["columns"]:
+                Table_Frecuences.treeview.column(col, anchor="center")
+
+            b=0
+            for key,value in Variables.items():
+                x_pos = 40+(215*b)
+                lab = Label(Window_Frecuences_Table , text=f"{key} = {value}" , font=("Times New Roman" , 12) , bg="#FEE1AB")
+                lab.place(x=x_pos , y=240)
+                b +=1
+        except (IndexError , ValueError, TypeError):
+            Error_Icon = PhotoImage(file="Images/error_icon.png")
+            Win_Err = Toplevel(Window_Frecuences_Table)
+            Win_Err.geometry("700x100+400+400")
+            Win_Err.title("Error")
+            Win_Err.iconphoto(False,Error_Icon)
+
+            Text = Label(Win_Err , text="Hubo un error al procesar los datos" , font=("Times New Roman" , 13) , justify=CENTER)
+            Text.place(x=240 , y=20)
+
+            Btn_Close = Button(Win_Err , text="Cerrar" , font=("Times New Roman" , 13) , command= Win_Err.destroy)
+            Btn_Close.place(x=330 , y=60)
+
+            Win_Err.protocol("WM_DELETE_WINDOW" , Win_Err.destroy)
+            Win_Err.grab_set()
+            Win_Err.resizable(False,False)
+            Win_Err.mainloop()
+    Window_Frecuences_Table = Toplevel(Main_Window)
+    Window_Frecuences_Table.geometry("1240x700+135+100")
+    Window_Frecuences_Table.title("Tabla de frecuencias")
+    Window_Frecuences_Table.config(bg="#6C6E72")
+    Window_Frecuences_Table.iconphoto(False , Icon)
+
+    # Grid = 16 rows , 16 columns
+    Data = StringVar(Window_Frecuences_Table)
+    Precision = IntVar(Window_Frecuences_Table)
+    
+    Title = Label(Window_Frecuences_Table , text="Calculo de Tablas de Frecuencia" , font=("Times New Roman" , 22), foreground="#ffffff", justify=CENTER , width=76 , bg="#9DAEC6" , highlightthickness=1 ,highlightbackground="#ffffff")
+    Title.place(x=9 , y=10)
+
+    Section_Input = Label(Window_Frecuences_Table , width=168 ,height=10, bg="#FEE1AB" , highlightthickness=2 , highlightbackground="#000000")
+    Section_Input.place(x=29 , y=65)
+
+    Text_Input_Data = Label(Window_Frecuences_Table , text="Ingrese los valores:", font=("Times New Roman" , 13) , bg="#FEE1AB")
+    Text_Input_Data.place(x=40 , y=90)
+    Input_Data = Entry(Window_Frecuences_Table , textvariable=Data , border=1 , cursor="xterm" , width=100 , font=("Courier New" , 13) , bg="#ffffff")
+    Input_Data.place(x=180 , y=90)
+    Input_Data.focus()
+
+    Text_Input_Precision_Results = Label(Window_Frecuences_Table , text="Precision:" , font=("Times New Roman" , 13) , bg="#FEE1AB")
+    Text_Input_Precision_Results.place(x=40 , y=130)
+    Input_Precision_Results = Entry(Window_Frecuences_Table , width=10 , textvariable=Precision , cursor="xterm" , font=("Courier New" , 13) , bg="#ffffff")
+    Input_Precision_Results.place(x=180 , y=130)
+
+    Btn_Calc_Table = Button(Window_Frecuences_Table , text="Generar Tabla" , font=("Times New Roman" , 15) , width=30 , bg="#F4B0C0" , command= lambda: Create_Table(Precision , Data , Table)) # Si no colocas lambda: o colocas parentesis a la funcion, esta se ejecuta cuando el boton se crea, y puede generar problemas
+    Btn_Calc_Table.place(x=600 , y=130)
+
+    Section_Frecuences_Table = Label(Window_Frecuences_Table , width=168 , height=30 , bg="#FEE1AB" , highlightthickness=2 , highlightbackground="#000000")
+    Section_Frecuences_Table.place(x=29 , y=180)
+
+    
+    Table = TreeviewFrame(Window_Frecuences_Table)
+    Table.config(width=10, height=0)
+    Table.place(x=40 , y=355)
+
+    Table.treeview.config(columns=("1", "2" ,"3", "4", "5", "6", "7", "8", "9", "10", "11") , show="headings")
+    Table.treeview.heading("1" , text="m")
+    Table.treeview.heading("2" , text="Li")
+    Table.treeview.heading("3" , text="Ls")
+    Table.treeview.heading("4" , text="Grupos")
+    Table.treeview.heading("5" , text="xi")
+    Table.treeview.heading("6" , text="fi")
+    Table.treeview.heading("7" , text="Fi")
+    Table.treeview.heading("8" , text="hi")
+    Table.treeview.heading("9" , text="Hi")
+    Table.treeview.heading("10" , text="hi%")
+    Table.treeview.heading("11" , text="HI%")
+
+    for a in range(1 , 12):
+        Table.treeview.column(f"{a}" , anchor="center" , width=104)
+
+    style = ttk.Style()
+    style.configure("Treeview.Heading" , font=("Arial" , 10))
+
+    style.map("Treeview",
+            foreground=[("selected", "black")],
+            background=[("selected", "skyblue")])
+
+    Btn_Back = Button(Window_Frecuences_Table , text="Volver", font=("Times New Roman" , 12) , command=Back_to_main_window , width=134 , bg="#F4B0C0")
+    Btn_Back.place(x=10 , y=660)
+
+    Window_Frecuences_Table.protocol("WM_DELETE_WINDOW" , Back_to_main_window)
+    Window_Frecuences_Table.resizable(False,False)
+    Window_Frecuences_Table.mainloop()
+
+def Create_Window_Samplings():
+    pass
+
+Main_Window = Tk()
+
+Image_Right_Section = PhotoImage(file="Images/normal_distribution.png")
+Image_Right_Section.subsample(10)
+Icon = PhotoImage(file="Images/icon.png")
+
+Main_Window.geometry("1240x700+135+100")
+Main_Window.title("Seleccion")
+Main_Window.config(bg="#9DAEC6")
+Main_Window.iconphoto(False,Icon)
+
+Right_Section = Label(Main_Window , width=531 , height=691 , bg="#F7EDCB" , image=Image_Right_Section , highlightthickness=2 , highlightbackground="#000000")
+Right_Section.place(x=700 , y=0)
+
+BtnFrecuencyTable = Button(Main_Window,text="Crear tabla de frecuencias", font=("Times New Roman", 14) , command=Create_Window_Frecuences_Table , width=30)
+BtnFrecuencyTable.place(x=190,y=300)
+
+Main_Window.resizable(False,False)
+Main_Window.mainloop()
+
+
+
+
